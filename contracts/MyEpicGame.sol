b2608e2af78709d9afb11960e06e0422170e6e76
Matthew Tang <matthew.tang@connect.hku.hk>
2021-12-14 00:14:44 +0800

    add defaultCharacters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,13 +2,54 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
 // contract
 
 contract MyEpicGame {
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    CharacterAttributes[] defaultCharacters;
+
     // constructor
-    constructor() {
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initialising %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
         console.log("myEpicGame constructed yo");
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,15 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Bojack", "Princess carolyn", "Mr Peanut butter"],
+    [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK5XchwUGs75xIyfJ5XJsemrOVM_DdJna9rw9PtEuEA3GMkqWuKI004sW-JCQequI6e_g&usqp=CAU", // Images
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTv35ki3PrwK3ikMDMBZ15dZY3LyEB3eWseyh8epi_1RCXEpekqGFSE4kfKzqZ-MLkCdQ&usqp=CAU",
+      "https://i.pinimg.com/474x/bf/0f/19/bf0f19dc0c6893fba26d8307dcfbef85--bojack-animation-character.jpg",
+    ],
+    [100, 200, 300],
+    [100, 50, 2]
+  );
   await gameContract.deployed();
   console.log("contract deployed to:", gameContract.address);
 };
