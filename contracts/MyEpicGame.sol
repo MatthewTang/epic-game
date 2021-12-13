37adbe9cfb622a4497718d2240bf332649dd469c
Matthew Tang <matthew.tang@connect.hku.hk>
2021-12-13 23:48:52 +0800

    add contract and hello-world runscript

---
+++ contracts/MyEpicGame.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+// contract
+
+contract MyEpicGame {
+    // constructor
+    constructor() {
+        console.log("myEpicGame constructed yo");
+    }
+}
